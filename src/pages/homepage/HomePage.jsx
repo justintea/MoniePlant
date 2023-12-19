f2644ef8c75f5ed117eb4d6548fda1954afeca7a
justintea <justin.tehhy@gmail.com>
2023-12-19 14:12:44 +0800

    20231219_p2_homepage content drafted

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,11 +1,37 @@
 export default function HomePage() {
     return (
       <>
-        <h1>This is the homepage!</h1>
-        <p>Where your financial dreams can come true</p>
-  
-  
-        
+        <h1>the Dividend Farmer</h1>
+
+        <h2> Why this app? </h2>
+        <p> This app/website is my passion project, designed to make finance engaging and accessible. It's a digital guide through the ever-evolving world of investments, providing bite-sized insights into the latest trends while uncovering the fascinating history of banking and finance. Think of it as your compass to demystify financial complexities and gain a richer understanding of the markets. Whether you're a seasoned investor or just starting, let's embark on this financial adventure together. Welcome to a space where learning about finance feels like discovering your own unique path through a captivating landscape. </p>
+        <p> With a special focus on dividend investing, we believe in the fundamental principles of returns and cash flow prioritization, harnessing the power of compounding interest to shape your financial journey. </p>
+
+        <h2>Background: evolution of banking & investing</h2>
+        <p>The realms of banking and investing have deep historical roots, evolving over centuries to become integral components of the global financial system. </p>
+        <p>Banking traces its origins to ancient civilizations, where temples and trusted entities served as depositories for valuable possessions. Over time, banking systems became more formalized. The Medici Bank in Florence during the Renaissance is often credited as one of the first modern banks, providing financial services such as loans and currency exchange. In the 17th century, the Bank of England was established, marking the birth of central banking. The 20th century witnessed significant developments, including the rise of commercial banks, investment banks, and the advent of electronic banking in the late 20th century. Today, banks play a crucial role in facilitating economic activities by offering various services such as deposits, loans, and wealth management. </p>
+        <p>Investing, as a practice, has evolved alongside the development of financial markets. The Dutch East India Company, formed in the 17th century, is often considered one of the earliest examples of a publicly traded company, introducing the concept of shares. The 19th century saw the emergence of stock exchanges, facilitating the buying and selling of securities. The modern concept of investing gained momentum in the 20th century, fueled by advancements in financial theory and technology. The establishment of mutual funds and the growth of individual investors contributed to the democratization of investing. Today, a wide array of investment vehicles, including stocks, bonds, real estate, and alternative investments, allows individuals and institutions to allocate capital with diverse risk-return profiles. </p>
+
+        <h2>Value investing</h2>
+        <p>Investors employ various strategies to navigate financial markets and achieve their financial goals. One prominent approach is value investing, where investors seek stocks perceived to be undervalued based on fundamental analysis. This involves assessing a company's intrinsic value relative to its market price, often focusing on factors like earnings, dividends, and financial health. </p>
+
+        <h2>Growth investing </h2>
+        <p> Growth investing, on the other hand, centers around identifying companies with high growth potential. Investors in growth stocks prioritize companies expected to outperform the market in terms of revenue and earnings growth, often disregarding current high valuations for future potential. </p>
+
+        <h2>Dividend investing </h2>
+        <p> Dividend investing involves targeting stocks with a history of paying consistent dividends. This strategy appeals to income-focused investors looking for a steady stream of cash flow. Dividend stocks are often associated with stable, mature companies with a history of profitability.  </p>
+
+        <h2> Fundamental analysis </h2>
+        <p> Investors also rely on fundamental analysis, evaluating a company's financial statements, earnings, and economic indicators to make informed investment decisions. This method aims to understand the intrinsic value of a security. </p>
+
+        <h2> Technical analysis </h2>
+        <p>Conversely, technical analysis involves studying historical price movements and trading volumes to forecast future price trends. This strategy assumes that historical market data can provide insights into potential future price movements. </p>
+
+        <p> Each strategy comes with its set of advantages and risks, and investors often combine multiple approaches to create a diversified portfolio aligned with their risk tolerance and investment objectives. </p>
+
+        <br></br>
+        <p style={{fontStyle: 'italic'}}>Powered by ChatGPT 3.5, prompts by Justin T</p>
+
       </>
     );
   }
