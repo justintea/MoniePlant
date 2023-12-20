1d98f4628cf6c179e45682c4bc878ed07d4850b3
justintea <justin.tehhy@gmail.com>
2023-12-20 22:02:20 +0800

    20231220_p2_search -> render -> Create in Airtable flow completed

--- src/pages/stockpickerpage/StockList.jsx
+++ src/pages/stockpickerpage/StockList.jsx
@@ -6,7 +6,9 @@ export default function StockList({myAppdata , stockData}) {
 
     return(
 <>
-<h2>Stocklist up</h2>
+<h2>Stocklist</h2>
+
+{stockData.map((item, idx) => (<Airtable name={stockData[0].name} ticker={stockData[0].ticker} price={stockData[0].price} div={stockData[0].div} freq={stockData[0].freq} />)) }
 
 <StockItem stockData={stockData} />
 
@@ -15,9 +17,9 @@ export default function StockList({myAppdata , stockData}) {
 }
 
 
-{/* {myAppdata.map((item)=> (<StockItem key={myAppdata[0].name} name={myAppdata[0].name} ticker={myAppdata[0].ticker} price={myAppdata[0].price} div={myAppdata[0].div} freq={myAppdata[0].freq} />))} */}
 
-// myAppdata.map((item, idx) => (<Airtable name={myAppdata[0].name} ticker={myAppdata[0].ticker} price={myAppdata[0].price} div={myAppdata[0].div} freq={myAppdata[0].freq} />))
+// {myAppdata.map((item, idx) => (<Airtable name={myAppdata[0].name} ticker={myAppdata[0].ticker} price={myAppdata[0].price} div={myAppdata[0].div} freq={myAppdata[0].freq} />))}
+{/* {myAppdata.map((item)=> (<StockItem key={myAppdata[0].name} name={myAppdata[0].name} ticker={myAppdata[0].ticker} price={myAppdata[0].price} div={myAppdata[0].div} freq={myAppdata[0].freq} />))} */}
 {/* price={myAppdata[0].price} div={myAppdata[0].div} freq={myAppdata[0].freq}  */}
 {/* {myAppdata.map((item)=> (<StockItem stockData={stockData} />))} */}
 {/* {myAppdata.map((item)=> (<StockItem stockData={stockData} />))} */}
